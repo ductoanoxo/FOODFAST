3a223e40fcb68d6bb0b475fca0b9d8393dbd9b5f
Kietnehi <149951917+Kietnehi@users.noreply.github.com>
2025-11-01 23:47:52 +0700

    TEST

--- admin_app/src/services/socketService.js
+++ admin_app/src/services/socketService.js
@@ -14,7 +14,7 @@ class SocketService {
 
 
         const SOCKET_URL =
-            import.meta.env.VITE_API_URL || 'http://localhost:5000';
+            import.meta.env.VITE_SOCKET_URL || 'http://localhost:5000';
 
         this.socket = io(SOCKET_URL, {
             auth: {
