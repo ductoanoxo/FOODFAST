60ab55564a862d6c2223676f1363fdd48f2f312d
Kietnehi <149951917+Kietnehi@users.noreply.github.com>
2025-11-13 21:33:31 +0700

    test

--- admin_app/src/__tests__/unit/redux/slices/authSlice.test.js
+++ admin_app/src/__tests__/unit/redux/slices/authSlice.test.js
@@ -4,7 +4,7 @@ import authReducer, { loginSuccess, logout } from '@/redux/slices/authSlice';
 describe('Admin authSlice', () => {
     let initialState;
 
-    beforeEach(() => {
+    beforeEach123123123123123123132(() => {
         // Clear localStorage mock
         localStorage.clear();
         vi.clearAllMocks();
