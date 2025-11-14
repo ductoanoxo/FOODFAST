96e529ae0b2ba9bef6a42f9cf06a6c155526bf82
Kietnehi <149951917+Kietnehi@users.noreply.github.com>
2025-11-15 01:13:02 +0700

    UPDATE PUBLIC ID

--- server_app/API/Controllers/paymentController.js
+++ server_app/API/Controllers/paymentController.js
@@ -14,7 +14,7 @@ const getBaseUrl = () => {
     }
     // Production server
     if (process.env.NODE_ENV === 'production') {
-        return 'http://98.90.205.114:3000/payment/vnpay/return'
+        return 'http://13.220.101.54:3000/payment/vnpay/return'
     }
     // Local development
     return 'http://localhost:3000/payment/vnpay/return'
